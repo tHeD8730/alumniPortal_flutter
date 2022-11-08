8ccc7b23844ea33f2df9a0bfdd043bbd2e5eff3e
Your Name <you@example.com>
2022-11-08 14:40:04 +0530

    splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,37 @@
-import 'package:alumni_portal/src/welcomePage.dart';
+import 'package:alumni_portal/src/screens/splashScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:get/get.dart';
+import 'package:alumni_portal/src/screens/welcomePage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AlumniPortal());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AlumniPortal extends StatefulWidget {
+  const AlumniPortal({Key? key}) : super(key: key);
 
+  @override
+  State<AlumniPortal> createState() => _AlumniPortalState();
+}
 
+class _AlumniPortalState extends State<AlumniPortal> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-       Navigator.push(
-            context, MaterialPageRoute(builder: (context) => WelcomePage()));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Sizer(
+      builder: (
+        context,
+        orientation,
+        deviceType,
+      ) {
+        return GetMaterialApp(
+          home: const SplashScreen(),
+          routes: {
+            'splash': (BuildContext context) => const SplashScreen(),
+            'welcome': (BuildContext context) => WelcomePage(),
+          },
+        );
+      },
     );
   }
 }
